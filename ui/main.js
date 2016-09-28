3ccecae312d189770e837f176d7c5db27a8110fd
nikitasharma1 <nikita.sh0410@gmail.com>
2016-09-28 17:14:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,3 +20,19 @@ button.onclick = function () {
     request.open ('GET', 'http://nikitasharma1.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+//Submit name 
+
+var nameInput = document.getElementById("name");
+var name = nameInput.value;
+submit.onclick = function() {
+  //Make a request to server and sendd the name
+  //Captre the names and render as list
+  var names = ['name1','name2','name3'];
+  var list = '';
+  for (var i=0; i<names.length; i++) {
+      list += '<li>' + names[i] + '</li>'
+  }
+  var ul = document.getElementById("namelist");
+  ul.innerHTML = list;
+};
