0296ce803c9da9030d8fdf1451f01dbfa5f22b01
nikitasharma1 <nikita.sh0410@gmail.com>
2016-09-28 16:34:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,10 +2,21 @@ console.log('Loaded!');
 var button = document.getElementById("counter");
 var counter = 0;
 button.onclick = function () {
-    //Make a request from counter endpoint
-    //capture the response and store it in a variable
-    //Render the variable in the correct span
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
+    //Create a request object
+    var request = new XMLHttpRequest();
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE){
+          // Take some action
+          if (request.status === 200) {
+              var counter = request.responseText;
+              var span = document.getElementById("count");
+              span.innerHTML = counter.toString();
+          }
+      }
+      //Not done yet
+    }; 
+    // Make a request
+    request.open ('GET', 'http://nikitasharma1.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
