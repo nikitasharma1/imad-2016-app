d1235c23a42dc660065972c1c40f47d89af4b3e7
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-26 17:15:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-// var Pool = require('pg').Pool;
-// var config = {
-//   host: 'db.imad.hasura-app.io',
-//   user: 'nikitasharma1',
-//   database: 'nikitasharma1',
-//   port: '5432',
-//   password: process.env.DB_PASSWORD
-// };
+var Pool = require('pg').Pool;
+var config = {
+  host: 'db.imad.hasura-app.io',
+  user: 'nikitasharma1',
+  database: 'nikitasharma1',
+  port: '5432',
+  password: process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -97,17 +97,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-//var pool = new Pool(config);
-// app.get('/', function (res, req) {
-//     pool.query('#', function (err, result) {
-//         if (err) {
-//             res.status(500).send(err.toString());
-//         }
-//         else {
-//             res.send(JSON.stringify(result));
-//         }
-//     });
-// });
+var pool = new Pool(config);
 
 var counter = 0;
 app.get('/counter', function(req, res) {
@@ -115,10 +105,24 @@ app.get('/counter', function(req, res) {
     res.send(counter.toString());
 });
 
-app.get('/:articleName', function (req, res) {
-  var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+app.get('/article/:articleName', function (req, res) {
+    var articleName = req.params.articleName;
+    pool.query('SELECT * from article  WHERE title = '+ articleName, function (err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+        }
+        else {
+            if (result.rows.length === 0) {
+                res.status(404).send(err.toString());
+            }
+            else {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
 });
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
