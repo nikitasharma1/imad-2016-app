f9f6873414f30b2be932c8652e19485f37130c0c
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-26 22:21:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,6 +80,24 @@ app.get('/article/:articleName', function (req, res) {
     });
 });
 
+app.get('/test/:articleName', function (req, res) {
+    var articleName = req.params.articleName;
+    pool.query("SELECT * from article  WHERE test = '"+ articleName + "'", function (err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+        }
+        else {
+            if (result.rows.length === 0) {
+                res.status(404).send(err.toString());
+            }
+            else {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
