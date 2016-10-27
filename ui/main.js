57c6ec9fe33e8f87a9d1a41eaf853cc3cfdf1f73
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-27 19:18:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,32 +3,16 @@ console.log('Loaded!');
 var button = document.getElementById("counter");
 var counter = 0;
 button.onclick = function () {
-    //Create a request object
     var request = new XMLHttpRequest();
-    //Capture the response and store it in a variable
     request.onreadystatechange = function () {
       if (request.readyState === XMLHttpRequest.DONE){
-          // Take some action
           if (request.status === 200) {
               var counter = request.responseText;
               var span = document.getElementById("count");
               span.innerHTML = counter.toString();
           }
       }
-      //Not done yet
     }; 
-    // Make a request
     request.open ('GET', 'http://nikitasharma1.imad.hasura-app.io/counter', true);
     request.send(null);
 };
-
-// var submit = document.getElementById("submit");
-// submit.onclick = function() {
-//   var names = ['name1','name2','name3'];
-//   var list = '';
-//   for (var i=0; i<names.length; i++) {
-//       list += '<li>' + names[i] + '</li>';
-//   }
-//   var ul = document.getElementById("namelist");
-//   ul.innerHTML = list;
-// };
