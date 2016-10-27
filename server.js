22e3ef391f94aac439384b276ba52fc15857ff5b
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-27 17:16:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,14 +63,15 @@ function createTemplate(data){
                         </div><!-- /.navbar-collapse -->
                       </div><!-- /.container-fluid -->
                     </nav>
+                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+                    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
                     <div id="div-height-60"></div>
                     <h3>${heading}</h3>
                     <div>
                         ${content}
                     </div>
                 </div>    
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-                <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+                
             </body>
         </html>
     `;
