87118e9ad93a2c9d26eb2d1c564ede1dc69f5ae0
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-27 18:36:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,6 +66,7 @@ function createTemplate(data){
                 </div>    
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
                 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+                <script type="text/javascript" src="/ui/main.js">
             </body>
         </html>
     `;
