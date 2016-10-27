c82b0fe0ad351137311e88bc1070481ef5d72475
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-27 19:00:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,14 +22,8 @@ button.onclick = function () {
     request.send(null);
 };
 
-//Submit name 
-
-// var nameInput = document.getElementById("name");
-// var name = nameInput.value;
 var submit = document.getElementById("submit");
 submit.onclick = function() {
-  //Make a request to server and sendd the name
-  //Captre the names and render as list
   var names = ['name1','name2','name3'];
   var list = '';
   for (var i=0; i<names.length; i++) {
@@ -38,14 +32,3 @@ submit.onclick = function() {
   var ul = document.getElementById("namelist");
   ul.innerHTML = list;
 };
-
-
-// $("#category").click(function(){
-//     var names = ['name1','name2','name3'];
-//     var list = '';
-//     for (var i=0; i<names.length; i++) {
-//       list += '<li>' + names[i] + '</li>';
-//     }
-//   $("#infoList").innerHTML = list;
-// });
-
