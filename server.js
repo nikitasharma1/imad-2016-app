a10d4a5b413793cb999c950cca682da3e6de2097
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-28 16:40:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,8 +96,7 @@ function createTemplate(data){
                     <div id="div-height-60"></div>
                     <h3>${heading}</h3>
                     <div>
-                        ${heading1(title)}<br/>
-                       
+                        ${heading1(title)}
                     </div>
                 </div>    
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
