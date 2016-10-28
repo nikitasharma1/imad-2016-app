e22d08d250a469e10dad4bb9a15dc8123cd6310e
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-29 00:48:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -129,24 +129,6 @@ app.get('/:articleName', function (req, res) {
     });
 });
 
-app.get('/:#divId', function (req, res) {
-    var divId = req.params.divId;
-    pool.query("SELECT * from article  WHERE title = '"+ divId + "'", function (err, result) {
-        if (err) {
-            res.status(500).send(err.toString());
-        }
-        else {
-            if (result.rows.length === 0) {
-                res.status(404).send(err.toString());
-            }
-            else {
-                var divData = result.rows[0];
-                res.send('hello');
-            }
-        }
-    });
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
