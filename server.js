3995e242b316643e27dbf4a559467e320448b50a
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-28 16:57:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,6 @@ function createTemplate(data){
                 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
                 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-                
             </head>
             <body>
                 <div class="container">
@@ -132,8 +131,6 @@ function createTemplate(data){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-
 
 var counter = 0;
 app.get('/counter', function(req, res) {
