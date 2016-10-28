7c9b52d9083d50424a1ec602cbd0b362141c5cbd
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-28 19:18:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -144,7 +144,7 @@ app.get('/counter', function(req, res) {
 });
 
 var pool = new Pool(config); 
-app.get('/article/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
     var articleName = req.params.articleName;
     pool.query("SELECT * from article  WHERE title = '"+ articleName + "'", function (err, result) {
         if (err) {
@@ -171,9 +171,9 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/hey ', function (req, res) {
-  res.send( counter);
-});
+// app.get('/ ', function (req, res) {
+//   res.send( counter);
+// });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
