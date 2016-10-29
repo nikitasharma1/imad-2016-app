c0d08f66b48310b8e22e1d019c758ff023a8b802
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-30 01:13:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -193,18 +193,18 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/ui/Slide:x', function (req, res) {
+app.get('/ui/:x', function (req, res) {
     var x = req.params.x;
-    res.sendFile(path.join(__dirname, 'ui', 'Slide'+x));
+    res.sendFile(path.join(__dirname, 'ui', x));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
+// app.get('/ui/style.css', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+// });
 
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
+// app.get('/ui/main.js', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+// });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
