3be53b7bb38d083f6135064de73265c5f3914337
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-30 13:56:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,20 @@ var articles = {
         content:  {
             info: [
                 {
-                    title: '<img class="slide img-responsive thumbnail" src="http://placehold.it/400x250" alt="ACADEMICS">',
+                    title: `<!--imported-->
+                    <div class="w3-content w3-display-container" style="max-width:800px">
+                      <img class="slide img-responsive thumbnail mySlides" src="img_nature_wide.jpg" style="width:100%">
+                      <img class="slide img-responsive thumbnail mySlides" src="img_fjords_wide.jpg" style="width:100%">
+                      <img class="slide img-responsive thumbnail mySlides" src="img_mountains_wide.jpg" style="width:100%">
+                      <div class="slide img-responsive thumbnail" w3-center w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
+                        <div class="w3-left w3-padding-left w3-hover-text-khaki" onclick="plusDivs(-1)">&#10094;</div>
+                        <div class="w3-right w3-padding-right w3-hover-text-khaki" onclick="plusDivs(1)">&#10095;</div>
+                        <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)"></span>
+                        <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(2)"></span>
+                        <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(3)"></span>
+                      </div>
+                    </div>
+                    '<img class="slide img-responsive thumbnail" src="http://placehold.it/400x250" alt="ACADEMICS">'`,
                     arr: [1, 2, 3]
                 },
                 {
