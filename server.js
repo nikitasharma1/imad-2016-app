8831d6a0efd38aa3beeca6e8dbaaa8cd9b395c6a
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-30 13:38:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -147,7 +147,7 @@ function createTemplate(data){
                 <div class="container-fluid">
                     <div id="div-height-50"></div>
                     <h3 class="text-center">${heading}</h3>
-                    <div id="${title}" class="container-fluid row">
+                    <div id="${title}" class="row">
                       <!--${content}-->
                       ${getContent(content)}
                     </div>
