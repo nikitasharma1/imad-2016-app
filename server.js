8ece6b0510c9a2d9b047f0a0d69702625d0947e1
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-30 13:52:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,6 +117,7 @@ function createTemplate(data){
                 <link href="/ui/style.css" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css?family=Molengo|Philosopher" rel="stylesheet"/>
                 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+                <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
                 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
             </head>
@@ -152,6 +153,7 @@ function createTemplate(data){
                       ${getContent(content)}
                     </div>
                 </div>    
+                <script type="text/javascript" src="/ui/main.js"></script>
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
                 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
             </body>
