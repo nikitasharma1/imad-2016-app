29f88d77e0c4f3a7d418b4d86182552277e7c387
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-30 14:36:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,25 +71,205 @@ var articles = {
                     arr: [1, 2, 3]
                 },
                 {
-                    title: `<img class="slide img-responsive thumbnail" src="http://placehold.it/400x250" alt="WEB DEVELOPMENT SKILLS">`,
-                    arr: [4, 5, 6]
+                    title: `
+                    
+                	<div id="myCarousel" class="carousel slide thumbnail" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                          <li data-target="#myCarousel" data-slide-to="1"></li>
+                          <li data-target="#myCarousel" data-slide-to="2"></li>
+                          <li data-target="#myCarousel" data-slide-to="3"></li>
+                        </ol>
+                    
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+                          <div class="item active">
+                            <img src="http://placehold.it/400x250" alt="Chania" width="460" height="345">
+                          </div>
+                    
+                          <div class="item">
+                            <img src="http://placehold.it/400x250" alt="Chania" width="460" height="345">
+                          </div>
+                        
+                          <div class="item">
+                            <img src="http://placehold.it/400x250" alt="Flower" width="460" height="345">
+                          </div>
+                    
+                          <div class="item">
+                            <img src="http://placehold.it/400x250" alt="Flower" width="460" height="345">
+                          </div>
+                        </div>
+                    
+                        <!-- Left and right controls -->
+                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                          <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                          <span class="sr-only">Next</span>
+                        </a>
+                      </div>		
+ 
+                	<script>
+                	$('.carousel').carousel({
+                      interval: 2000
+                    });
+                	</script>
+                    `,
+                    arr: [1, 2, 3]
                 },
                 {
-                    title: `<img class="slide img-responsive thumbnail" src="http://placehold.it/400x250" alt="PROJECTS">`,
-                    arr: [7, 8, 9]
+                    title: `
+                    
+                	<div id="myCarousel" class="carousel slide thumbnail" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                          <li data-target="#myCarousel" data-slide-to="1"></li>
+                          <li data-target="#myCarousel" data-slide-to="2"></li>
+                          <li data-target="#myCarousel" data-slide-to="3"></li>
+                        </ol>
+                    
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+                          <div class="item active">
+                            <img src="http://placehold.it/400x250" alt="Chania" width="460" height="345">
+                          </div>
+                    
+                          <div class="item">
+                            <img src="http://placehold.it/400x250" alt="Chania" width="460" height="345">
+                          </div>
+                        
+                          <div class="item">
+                            <img src="http://placehold.it/400x250" alt="Flower" width="460" height="345">
+                          </div>
+                    
+                          <div class="item">
+                            <img src="http://placehold.it/400x250" alt="Flower" width="460" height="345">
+                          </div>
+                        </div>
+                    
+                        <!-- Left and right controls -->
+                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                          <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                          <span class="sr-only">Next</span>
+                        </a>
+                      </div>		
+ 
+                	<script>
+                	$('.carousel').carousel({
+                      interval: 2000
+                    });
+                	</script>
+                    `,
+                    arr: [1, 2, 3]
                 },
                 {
-                    title: `<img class="slide img-responsive thumbnail" src="http://placehold.it/400x250" alt="CERTIFICATIONS">`,
-                    arr: [10, 11, 12]
+                    title: `
+                    
+                	<div id="myCarousel" class="carousel slide thumbnail" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                          <li data-target="#myCarousel" data-slide-to="1"></li>
+                          <li data-target="#myCarousel" data-slide-to="2"></li>
+                          <li data-target="#myCarousel" data-slide-to="3"></li>
+                        </ol>
+                    
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+                          <div class="item active">
+                            <img src="http://placehold.it/400x250" alt="Chania" width="460" height="345">
+                          </div>
+                    
+                          <div class="item">
+                            <img src="http://placehold.it/400x250" alt="Chania" width="460" height="345">
+                          </div>
+                        
+                          <div class="item">
+                            <img src="http://placehold.it/400x250" alt="Flower" width="460" height="345">
+                          </div>
+                    
+                          <div class="item">
+                            <img src="http://placehold.it/400x250" alt="Flower" width="460" height="345">
+                          </div>
+                        </div>
+                    
+                        <!-- Left and right controls -->
+                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                          <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                          <span class="sr-only">Next</span>
+                        </a>
+                      </div>		
+ 
+                	<script>
+                	$('.carousel').carousel({
+                      interval: 2000
+                    });
+                	</script>
+                    `,
+                    arr: [1, 2, 3]
                 },
                 {
-                    title: `<img class="slide img-responsive thumbnail" src="http://placehold.it/400x250" alt="OTHER SKILLS">`,
-                    arr: [13, 14, 16]
-                },
-                {
-                    title: `<img class="slide img-responsive thumbnail" src="http://placehold.it/400x250" alt="OTHER SKILL">`,
-                    arr: [17, 18, 19]
-                }
+                    title: `
+                    
+                	<div id="myCarousel" class="carousel slide thumbnail" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                          <li data-target="#myCarousel" data-slide-to="1"></li>
+                          <li data-target="#myCarousel" data-slide-to="2"></li>
+                          <li data-target="#myCarousel" data-slide-to="3"></li>
+                        </ol>
+                    
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+                          <div class="item active">
+                            <img src="http://placehold.it/400x250" alt="Chania" width="460" height="345">
+                          </div>
+                    
+                          <div class="item">
+                            <img src="http://placehold.it/400x250" alt="Chania" width="460" height="345">
+                          </div>
+                        
+                          <div class="item">
+                            <img src="http://placehold.it/400x250" alt="Flower" width="460" height="345">
+                          </div>
+                    
+                          <div class="item">
+                            <img src="http://placehold.it/400x250" alt="Flower" width="460" height="345">
+                          </div>
+                        </div>
+                    
+                        <!-- Left and right controls -->
+                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                          <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                          <span class="sr-only">Next</span>
+                        </a>
+                      </div>		
+ 
+                	<script>
+                	$('.carousel').carousel({
+                      interval: 2000
+                    });
+                	</script>
+                    `,
+                    arr: [1, 2, 3]
+                },                
             ]
         }
     },
