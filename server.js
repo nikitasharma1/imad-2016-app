713bfd74fcb0a810d3f1615225aa4cdb5f02a941
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-30 15:43:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,13 +24,7 @@ var articles = {
                     title: `
                     
                 	<div id="myCarousel" class="carousel slide thumbnail" data-ride="carousel">
-                        <!-- Indicators -->
-                        <ol class="carousel-indicators">
-                          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                          <li data-target="#myCarousel" data-slide-to="1"></li>
-                          <li data-target="#myCarousel" data-slide-to="2"></li>
-                          <li data-target="#myCarousel" data-slide-to="3"></li>
-                        </ol>
+                        
                     
                         <!-- Wrapper for slides -->
                         <div class="carousel-inner" role="listbox">
