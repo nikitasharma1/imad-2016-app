c5162ec5d030adf6b4748f5972351fc6de34b538
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-30 14:59:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -53,60 +53,11 @@ var articles = {
                     
                         <!-- Left and right controls -->
                         <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                          <span class="glyphicon glyphicon-menu-left btn-sm" aria-hidden="true"></span>
+                          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
                           <span class="sr-only">Previous</span>
                         </a>
                         <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                          <span class="glyphicon glyphicon-menu-right btn-sm" aria-hidden="true"></span>
-                          <span class="sr-only">Next</span>
-                        </a>
-                      </div>		
- 
-                	<script>
-                	$('.carousel').carousel({
-                      interval: 2000
-                    });
-                	</script>
-                    `,
-                    arr: [1, 2, 3]
-                },
-                {
-                    title: `
-                    
-                	<div id="myCarousel" class="carousel slide thumbnail" data-ride="carousel">
-                        <!-- Indicators -->
-                        <ol class="carousel-indicators">
-                          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                          <li data-target="#myCarousel" data-slide-to="1"></li>
-                          <li data-target="#myCarousel" data-slide-to="2"></li>
-                          <li data-target="#myCarousel" data-slide-to="3"></li>
-                        </ol>
-                    
-                        <!-- Wrapper for slides -->
-                        <div class="carousel-inner" role="listbox">
-                          <div class="item active">
-                            <img src="http://placehold.it/400x250" alt="Chania" width="460" height="345">
-                          </div>
-                    
-                          <div class="item">
-                            <img src="http://placehold.it/400x250" alt="Chania" width="460" height="345">
-                          </div>
-                        
-                          <div class="item">
-                            <img src="http://placehold.it/400x250" alt="Flower" width="460" height="345">
-                          </div>
-                    
-                          <div class="item">
-                            <img src="http://placehold.it/400x250" alt="Flower" width="460" height="345">
-                          </div>
-                        </div>
-                    
-                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                          <span class="glyphicon glyphicon-menu-left btn-sm" aria-hidden="true"></span>
-                          <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                          <span class="glyphicon glyphicon-menu-right btn-sm" aria-hidden="true"></span>
+                          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
                           <span class="sr-only">Next</span>
                         </a>
                       </div>		
@@ -152,11 +103,11 @@ var articles = {
                     
                         <!-- Left and right controls -->
                         <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                          <span class="glyphicon glyphicon-menu-left btn-sm" aria-hidden="true"></span>
+                          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
                           <span class="sr-only">Previous</span>
                         </a>
                         <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                          <span class="glyphicon glyphicon-menu-right btn-sm" aria-hidden="true"></span>
+                          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
                           <span class="sr-only">Next</span>
                         </a>
                       </div>		
@@ -202,11 +153,11 @@ var articles = {
                     
                         <!-- Left and right controls -->
                         <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                          <span class="glyphicon glyphicon-menu-left btn-sm" aria-hidden="true"></span>
+                          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
                           <span class="sr-only">Previous</span>
                         </a>
                         <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                          <span class="glyphicon glyphicon-menu-right btn-sm" aria-hidden="true"></span>
+                          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
                           <span class="sr-only">Next</span>
                         </a>
                       </div>		
@@ -252,11 +203,61 @@ var articles = {
                     
                         <!-- Left and right controls -->
                         <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                          <span class="glyphicon glyphicon-menu-left btn-sm" aria-hidden="true"></span>
+                          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
                           <span class="sr-only">Previous</span>
                         </a>
                         <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                          <span class="glyphicon glyphicon-menu-right btn-sm" aria-hidden="true"></span>
+                          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                          <span class="sr-only">Next</span>
+                        </a>
+                      </div>		
+ 
+                	<script>
+                	$('.carousel').carousel({
+                      interval: 2000
+                    });
+                	</script>
+                    `,
+                    arr: [1, 2, 3]
+                },
+                {
+                    title: `
+                    
+                	<div id="myCarousel" class="carousel slide thumbnail" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                          <li data-target="#myCarousel" data-slide-to="1"></li>
+                          <li data-target="#myCarousel" data-slide-to="2"></li>
+                          <li data-target="#myCarousel" data-slide-to="3"></li>
+                        </ol>
+                    
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+                          <div class="item active">
+                            <img src="http://placehold.it/400x250" alt="Chania" width="460" height="345">
+                          </div>
+                    
+                          <div class="item">
+                            <img src="http://placehold.it/400x250" alt="Chania" width="460" height="345">
+                          </div>
+                        
+                          <div class="item">
+                            <img src="http://placehold.it/400x250" alt="Flower" width="460" height="345">
+                          </div>
+                    
+                          <div class="item">
+                            <img src="http://placehold.it/400x250" alt="Flower" width="460" height="345">
+                          </div>
+                        </div>
+                    
+                        <!-- Left and right controls -->
+                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                          <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
                           <span class="sr-only">Next</span>
                         </a>
                       </div>		
