dc6970fd96d25ca6cf4802bdabbbbedc9e6a2641
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-30 11:18:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ function createTemplate(data){
             }
             else 
             {
-                out += '<div class="container-fluid"><ul id="info">';
+                out += '<div class="container-fluid" id="info-div"><ul id="info">';
                 for(var i in content1.info) {
                 out += '<li class="text-center col-sm-4" id="slide-container">'+content1.info[i].title+'</li>';
                 }
