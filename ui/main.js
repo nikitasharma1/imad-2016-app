09170bfb8f68803cf4eff5fafbe46f8126f7da66
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-31 14:21:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,3 +17,62 @@ button.onclick = function () {
     request.send(null);
 };
 
+$(document).ready(function(){
+  $('.sub-div-1').hide();
+  $('.sub-div-2').hide();
+  $('.btn-close').hide();
+  // $('#arrow-').hide(); 
+  $('#arrow-2').hide(); 
+  
+    $('#arrow-1').click(function(){
+    $('.image-div').addClass('col-md-6');
+    $('.sub-div-1').show();
+    $('#arrow-2').show(); 
+    $('#arrow-1').hide(); 
+    // $('.btn-close').addClass("close-sub-div-1");
+    // $('.btn-close').show();
+  });
+
+  $('#arrow-2').click(function(){
+    
+    $('.image-div').removeClass('col-md-6');
+    $('.image-div').addClass('col-md-4');
+    $('.sub-div-1').removeClass('col-md-6');
+    $('.sub-div-1').addClass('col-md-4');
+    $('.sub-div-2').show();
+    $('.btn-close').show();
+    $('#arrow-2').hide(); 
+    // $('.btn-close').removeClass('close-sub-div-1');
+    // $('.btn-close').addClass('.close-sub-divs');
+  });
+
+  
+  $('.btn-close').click(function(){
+    $('.sub-div-1').hide();
+    $('.sub-div-2').hide();
+    $('.btn-close').hide();
+    $('.sub-div-1').removeClass('col-md-4');
+    $('.sub-div-1').addClass('col-md-6');
+    $('.image-div').removeClass('col-md-4');
+    $('#arrow-1').show(); 
+  });
+
+//   $(".close-sub-div-1").click(function(){
+//     $('.sub-div-1').hide();
+//     $('.image-div').removeClass('col-md-6');
+
+//     $(this).hide();
+//     $(this).removeClass('close-sub-div-1');  
+//   });
+
+//   $('.close-sub-divs').click(function(){
+//     $('.sub-div-2').hide();
+//     $('.sub-div-1').removeClass('col-md-4');
+//     $('.sub-div-1').addClass('col-md-6');
+//     $('.image-div').removeClass('col-md-4');
+//     $('.image-div').addClass('col-md-6');
+//     $(this).addClass('close-sub-div-1');
+//     $(this).removeClass('close-sub-divs');
+//   });
+});
+
