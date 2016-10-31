410be4864b2dadbd714e4e892d5438d5ec79aa47
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-31 21:04:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,3 +12,9 @@ $(document).ready(function(){
     });
 });
 
+document.getElementById('send').onclick(function () {
+    var email = document.getElementById('email').value;
+    var subject = document.getElementById('subject').value;
+    var message = document.getElementById('message').value;
+    console.log("sent");
+});
