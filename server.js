2e43d05ecbac58f960173ca2d684750c7b37d4e9
nikitasharma1 <nikita.sh0410@gmail.com>
2016-11-01 00:12:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -420,14 +420,12 @@ app.post('/contact', function(req, res){
     var email = req.body.email;
     var subject = req.body.subject;
     var message = req.body.message;
-    var post  = {email: req.body.email, subject: req.body.subject, message: req.body.message};
+    var post  = {email: email, subject: subject, message: message};
         pool.query('INSERT INTO contact SET ?', post, function(err, result) {
            if (err) throw err;
         else res.send('success');
         });
-    //console.log(req.body.email);
-    // pool.query("INSERT INTO contact (email, subject, message) VALUES ('"+email+"','"+subject+"','"+message+"')");
-    // pool.query("INSERT into contact (email, subject, message) values ("+email+","+subject+","+message+")");
+    
 });
 
 
