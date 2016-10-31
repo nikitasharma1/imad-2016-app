af1a1b5a282f9c227846c0b47a508d57a2f1a694
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-31 19:45:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -246,7 +246,7 @@ var articles = {
                         </div>
                     
                         <!-- Left and right controls -->
-                        <a class="left carousel-control" href="#myCarousel${+i}" role="button" data-slide="prev">
+                        <a class="left carousel-control" href="#myCarousel5" role="button" data-slide="prev">
                           <span class="glyphicon glyphicon-chevron-left btn-sm" aria-hidden="true"></span>
                           <span class="sr-only">Previous</span>
                         </a>
