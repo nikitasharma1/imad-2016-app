d1f29d1abc2d12a3a06649e72456d9a0081c57a7
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-31 19:16:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -291,7 +291,8 @@ var articles = {
     'portfolio': {
         title: 'My Portfolio',
         heading: 'My Portfolio',
-        content:  [`<img class="image image1 slide img-responsive thumbnail" src="http://placehold.it/400x250"/>`,
+        content:  [`<img class="image image1 slide img-responsive thumbnail" src="http://placehold.it/400x250"/>
+                    <img class="image sub-image slide img-responsive thumbnail" src="http://placehold.it/400x250"/>`,
                    `<img class="image image1 slide img-responsive thumbnail" src="http://placehold.it/400x250"/>`,
                    `<img class="image image1 slide img-responsive thumbnail" src="http://placehold.it/400x250"/>`,
                    `<img class="image image1 slide img-responsive thumbnail" src="http://placehold.it/400x250"/>`,
