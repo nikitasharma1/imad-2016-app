81e70c04293b18a61ccbc399d099698246e15670
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-31 19:07:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     $('sub-image').hide();
     $('.image1').click(function(){
-       $(this).addClass('col-md-6');
        $(this).siblings().show();
     });
     $('.sub-image').click(function(){
