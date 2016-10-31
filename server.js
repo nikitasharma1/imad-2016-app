3ab1ff916e3097ba125cd886cddffb4c4ef68bd4
nikitasharma1 <nikita.sh0410@gmail.com>
2016-11-01 00:17:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -428,6 +428,23 @@ var pool = new Pool(config);
     
 // });
 
+app.post('/contact', function (req, res) {
+    
+    pool.query("Insert into contact (email, subject, message) values ('test@test.com', 'test', 'test')", function (err, result) {
+        // if (err) {
+        //     res.status(500).send(err.toString());
+        // }
+        // else {
+        //     if (result.rows.length === 0) {
+        //         res.status(404).send(err.toString());
+        //     }
+        //     else {
+        //         var articleData = result.rows[0];
+        //         res.send(createTemplate(articleData));
+        //     }
+        // }
+    });
+});
 
 
 
