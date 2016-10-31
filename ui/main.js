07cbc274bbd94c59ad116558b550201882ee69e2
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-31 21:45:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,9 +12,3 @@ $(document).ready(function(){
     });
 });
 
-var send = document.getElementById('send').onclick(function () {
-  var email = document.getElementById('email').value;
-  var subject = document.getElementById('subject').value;
-  var message = document.getElementById('message').value;
-  pool.query("INSERT into contact (email, subject, message) values (?, ,?, ?)", [email, subject, message]);
-});
