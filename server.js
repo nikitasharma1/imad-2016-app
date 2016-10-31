339da09b649ff486ca78dc62872e5fb4724a05e0
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-31 21:43:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -414,11 +414,11 @@ app.get('/counter', function(req, res) {
 });
 
 var pool = new Pool(config); 
-var email;
-var subject;
-var message;
+// var email;
+// var subject;
+// var message;
 app.get('/contact?', function(req, res) {
-    pool.query("INSERT into contact (email, subject, message) values (?, ,?, ?)", [email, subject, message]);
+    
 });
 
 app.get('/:articleName', function (req, res) {
