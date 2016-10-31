b4da2a4b36a9112f42342b71243b40550812a72d
nikitasharma1 <nikita.sh0410@gmail.com>
2016-10-31 14:28:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -306,6 +306,9 @@ var articles = {
                       <span class="btn-close glyphicon glyphicon-remove btn-lg"></span>
                     </div> 
                     </div>
+                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+                    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+                    <script type="text/javascript" src="/ui/main.js"></script>
                     `
                 	]
     },
