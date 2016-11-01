86083a5bb583d980d9a2188d605f51dd678ad3bf
nikitasharma1 <nikita.sh0410@gmail.com>
2016-11-01 16:16:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var bodyParser = require('body-parser');
+//var bodyParser = require('body-parser');
 
 // var Pool = require('pg').Pool;
 // var config = {
