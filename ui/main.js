8ecfaeb847169d108d271c8a3621a8d0db9b5945
nikitasharma1 <nikita.sh0410@gmail.com>
2016-11-01 16:20:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,9 +7,9 @@ $(document).ready(function(){
        $(this).hide(); 
     });
     
-    // $('.carousel').carousel({
-    //   interval: 20000
-    // });
+    $('.carousel').carousel({
+      interval: 200000
+    });
 });
 
 // var send = $('#send').click(function(){
