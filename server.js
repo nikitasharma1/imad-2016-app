be66f207b387ede08881bf002cc101631e66f66d
nikitasharma1 <nikita.sh0410@gmail.com>
2016-11-02 15:56:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -342,19 +342,7 @@ var articles = {
     'contact': {
         title: 'Contact Me',
         heading: 'Contact Me',
-        content:   `<form method="POST" action="/contact">
-                        <div class="col-md-2"></div>
-                        <div class="col-md-8">
-                            <label>Your Email</label><br/>
-                            <input type="email" class="form-control pad name="email" id="email"/><br/>                                
-                            <label>Subject</label><br/>
-                            <input type="text" class="form-control pad name="subject" id="subject"/><br/>                                
-                            <label>Message</label><br/>
-                            <textarea class="form-control" rows="10" name="message" id="message"></textarea><br/>
-                            <input class="btn btn-primary" id="send" value="Send"/><br/>
-                        </div>
-                        <div class="col-md-2"></div>
-                    </form>`
+        content:   ``
     },
 };    
 
