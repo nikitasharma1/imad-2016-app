0d3533a1f7ad6488234f39bc5e6fc31fd8f7c7fb
nikitasharma1 <nikita.sh0410@gmail.com>
2016-11-02 13:57:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,54 +21,7 @@ var articles = {
         heading: 'About Me',
         content:  {
             info: [
-                {
-                    title: `
-                    
-                	<div id="myCarousel" class="carousel slide thumbnail" data-ride="carousel" data-interval="false">
-                        
-                    
-                        <!-- Wrapper for slides -->
-                        <div class="carousel-inner" role="listbox">
-                          <div class="item active" >
-                            <img src="/ui/skills/skills.jpg" alt=" MY SKILLS" >
-                          </div>
-                    
-                          <div class="item">
-                            <img src="/ui/skills/practica.png" alt=" HTML5, CSS3, JAVASCRIPT" >
-                          </div>
-                        
-                          <div class="item">
-                            <img src="/ui/skills/jquery_bootstrap.png" alt=" JQUERY, BOOTSTRAP" >
-                          </div>
-                    
-                          <div class="item">
-                            <img src="/ui/skills/database.png" alt=" MYSQL, POSGRESQL" >
-                          </div>
-                          
-                          <div class="item">
-                            <img src="/ui/skills/phpnodejs.png" alt=" PHP, NODE.JS" >
-                          </div>
-                          
-                          <div class="item">
-                            <img src="/ui/skills/react-express.png" alt=" REACTJS, EXPRESS.JS" >
-                          </div>
-                          
-                        </div>
-                    
-                        <!-- Left and right controls -->
-                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                          <span class="glyphicon glyphicon-chevron-left btn-sm" aria-hidden="true"></span>
-                          <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                          <span class="glyphicon glyphicon-chevron-right btn-sm" aria-hidden="true"></span>
-                          <span class="sr-only">Next</span>
-                        </a>
-                    </div>		
- 
-                    `
-                    
-                },
+                
                 {
                     title: `
                     
